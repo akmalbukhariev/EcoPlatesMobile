1da7369ab431662467e227f56e8d5453042344f5
akmalbukhariev <akmalbukhariev@gmail.com>
2025-03-12 22:57:55 +0900

    added new images and start to create content view. Also remove under line of the Entry

--- EcoPlatesMobile/App.xaml.cs
+++ EcoPlatesMobile/App.xaml.cs
@@ -1,15 +1,29 @@
-﻿namespace EcoPlatesMobile
+﻿using EcoPlatesMobile.Views.User.Pages;
+
+using Microsoft.Maui.Platform;
+
+namespace EcoPlatesMobile
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+
+#if ANDROID
+            Microsoft.Maui.Handlers.EntryHandler.Mapper
+                .AppendToMapping(nameof(Entry),
+                        (handler, view) =>
+            {
+            handler.PlatformView.BackgroundTintList =
+            Android.Content.Res.ColorStateList.ValueOf(Colors.Transparent.ToPlatform());
+            });
+#endif
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AppShell());
+            return new Window(new UserRegistrationPage());
         }
     }
 }
