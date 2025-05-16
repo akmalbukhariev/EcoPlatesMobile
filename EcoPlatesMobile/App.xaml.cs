01c39be8bfe218b362637f672da59d1ef543069b
Buxariyev Akmal <akmalbukhariev@gmail.com>
2025-05-16 17:39:37 +0900

    Access token has been updated

--- EcoPlatesMobile/App.xaml.cs
+++ EcoPlatesMobile/App.xaml.cs
@@ -21,8 +21,8 @@ namespace EcoPlatesMobile
         protected override Window CreateWindow(IActivationState? activationState)
         {
             //return new Window(new NavigationPage(new LoginPage())); 
-            return new Window(new AppUserShell());
-            //return new Window(new DetailProductPage());
+            //return new Window(new AppUserShell());
+            return new Window(new AppCompanyShell());
         }
 
         private void RegisterRoutes()
