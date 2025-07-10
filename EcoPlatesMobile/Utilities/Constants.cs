df190f5719904d9416c5a44312c3c74e394ddfaa
akmalbukhariev <akmalbukhariev@gmail.com>
2025-07-10 22:40:16 +0900

    set all colors for pages and text

--- EcoPlatesMobile/Utilities/Constants.cs
+++ EcoPlatesMobile/Utilities/Constants.cs
@@ -6,12 +6,12 @@ namespace EcoPlatesMobile.Utilities
     internal class Constants
     {
         //public const string BASE_USER_URL = "http://www.ecoplates.uz:8080/user/";
-        //public const string BASE_USER_URL = "http://192.168.219.181:8083";
-        public const string BASE_USER_URL = "http://10.0.2.2:8083";
+        public const string BASE_USER_URL = "http://192.168.219.145:8083";
+        //public const string BASE_USER_URL = "http://10.0.2.2:8083";
         
         //public const string BASE_COMPANY_URL = "http://www.ecoplates.uz:8080/company/";
-        //public const string BASE_COMPANY_URL = "http://192.168.219.181:8081";
-        public const string BASE_COMPANY_URL = "http://10.0.2.2:8081";
+        public const string BASE_COMPANY_URL = "http://192.168.219.145:8081";
+        //public const string BASE_COMPANY_URL = "http://10.0.2.2:8081";
 
         public static readonly string Version = AppInfo.Current.VersionString;     // e.g., "1.0"
         public static readonly string Build = AppInfo.Current.BuildString;         // e.g., "1.0.0"
